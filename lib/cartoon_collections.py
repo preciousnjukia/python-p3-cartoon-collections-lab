ebf4def24e3cbdcdc22a8b97b114704a0883496e
preciousnjukia <preciousnjukia@gmail.com>
2023-10-11 16:10:19 +0300

    Assignment done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+     for i, name in enumerate(names, start=1):
+        print(f"{i}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeeter_calls):
+    modified_calls = []
 
-def long_planeteer_calls():
-    pass
+    for call in planeeter_calls:
+        modified_call = call.capitalize() + "!"
+        modified_calls.append(modified_call)
 
-def find_the_cheese():
-    pass
+    return modified_calls
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+         return True
+        
+    return False
+
+def find_the_cheese(ingredients):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if ingredient in cheese_types:
+            return ingredient
+    return None
